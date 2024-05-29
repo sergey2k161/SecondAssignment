13685c65f7d0bf98504b8c16526f447de0d1782e
sergey2k161 <joysergey4@gmail.com>
2024-05-29 09:22:47 +0300

    TaskService.cs problem

---
+++ SecondAssignment/Models/TaskService.cs
@@ -0,0 +1,51 @@
+﻿using SecondAssignment.Servise;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecondAssignment.Models;
+
+public class TaskService : ITaskService
+{
+    private readonly ApplicationDbContext _context;
+
+    public TaskService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Tasks>> GetTasksAsync()
+    {
+        return await _context.Tasks.ToListAsync();
+    }
+
+    public async Task<Tasks> GetTaskByIdAsync(int taskId)
+    {
+        return await _context.Tasks.FindAsync(taskId);
+    }
+
+    public async Task<Tasks> CreateTaskAsync(Tasks task)
+    {
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
+        return task;
+    }
+
+    public async Task<Tasks> UpdateTaskAsync(int taskId, Tasks task)
+    {
+        _context.Entry(task).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return task;
+    }
+
+    public async Task<bool> DeleteTaskAsync(int taskId)
+    {
+        var task = await _context.Tasks.FindAsync(taskId);
+        if (task == null)
+        {
+            return false;
+        }
+
+        _context.Tasks.Remove(task);
+        await _context.SaveChangesAsync();
+        return true;
+    }    
+}
